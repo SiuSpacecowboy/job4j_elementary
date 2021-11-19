ecf3c41b1147d730af6399754041fe3d375ba0b7
SiuSpacecowboy <danvitaly14@mail.ru>
2021-11-19 20:14:52 +0300

    Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,4 +1,4 @@
-package ru.job4j.ru.job4j.ru.job4j;
+package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
@@ -12,5 +12,4 @@ public class Multiple {
         System.out.println("1*8 = 8");
         System.out.println("1*9 = 9");
     }
-
 }
