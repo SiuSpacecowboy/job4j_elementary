2604b8948d25b293546d7353a6435a8821535df9
SiuSpacecowboy <danvitaly14@mail.ru>
2021-11-19 20:10:11 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j.ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vitaly Danyukovas");
+        System.out.println("12.09.1999");
+
+    }
+}
