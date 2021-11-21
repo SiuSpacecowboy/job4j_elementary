1de43a0ffe033ee154f0d17561398abd66f4a556
SiuSpacecowboy <danvitaly14@mail.ru>
2021-11-22 02:30:33 +0300

    0. Что такое переменная. [#3610 #231224]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,14 +2,12 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        System.out.println("1*1 = 1.0");
-        System.out.println("1*2 = 2");
-        System.out.println("1*3 = 3");
-        System.out.println("1*4 = 4");
-        System.out.println("1*5 = 5");
-        System.out.println("1*6 = 6");
-        System.out.println("1*7 = 7");
-        System.out.println("1*8 = 8");
-        System.out.println("1*9 = 9");
+        int age = 18;
+        System.out.println(age);
+        float length = 78.0332f;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(baby);
+
     }
 }
