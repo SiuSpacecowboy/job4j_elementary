a110dac29a6ced42483b9f7eef50322c89b4557d
SiuSpacecowboy <danvitaly14@mail.ru>
2021-11-21 16:51:19 +0300

    0. Что такое переменная. [#3610]

---
+++ src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Calculate {
+    public static void main(String[] args) {
+        int age = 18;
+        System.out.println(age);
+        float length = (float) 78.0332;
+        System.out.println(length);
+         boolean baby = true;
+        System.out.println(baby);
+
+    }
+
+}
