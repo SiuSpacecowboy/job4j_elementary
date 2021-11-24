3492a1623e915c4ea13a81f7fbce29e6cded2126
SiuSpacecowboy <danvitaly14@mail.ru>
2021-11-24 11:57:48 +0300

    2. Тесты для задачи "идеальный вес" [#159458 #232060]

--- src/test/java/ru/job4j/converter/WeightTest.java
+++ src/test/java/ru/job4j/converter/WeightTest.java
@@ -10,7 +10,7 @@ public class WeightTest {
     @Test
     public void manWeight() {
         short tall = 178;
-        double exp = 89.6999999;
+        double exp = 89.70;
         double real = Weight.manWeight(tall);
         double eps = 0.01;
         Assert.assertEquals(exp, real, eps);
