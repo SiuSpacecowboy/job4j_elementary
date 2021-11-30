25f0fb43c1e345e728dae60edcb1001d2be70fee
SiuSpacecowboy <danvitaly14@mail.ru>
2021-12-01 01:37:25 +0300

    5.7. Ипотека [#156315 #234457]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,11 +3,10 @@ package ru.job4j.loop;
 public class Mortgage {
 
     public static int year(double amount, double salary, double percent) {
-        int y = 1;
-        double a = amount + amount * (percent / 100);
-        while (a - salary > 0) {
-            a = (a - salary) + (a - salary) * (percent / 100);
-            y += 1;
+        int y = 0;
+        while (amount > 0) {
+            amount += amount * (percent / 100) - salary;
+            y++;
         }
         return y;
     }
