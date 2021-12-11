9ec61969b50122d08d0b18e34e7e2137068a6299
SiuSpacecowboy <danvitaly14@mail.ru>
2021-12-11 20:07:51 +0300

    6.5.1. Поиск минимального числа в массиве. [#179803 #238854]

--- src/main/java/ru/job4j/array/Min.java
+++ src/main/java/ru/job4j/array/Min.java
@@ -4,8 +4,8 @@ public class Min {
 
     public static int findMin(int[] array) {
         int min = array[0];
-        for (int index = 1; index < array.length; index++) {
-            if (array[index] < array[index - 1]) {
+        for (int index = 0; index < array.length; index++) {
+            if (min > array[index]) {
                 min = array[index];
             }
         }
--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -7,41 +7,41 @@ import static org.junit.Assert.*;
 
 public class FindLoopTest {
 
-    @Test
-    public void whenArrayHas5Then0() {
-        int[] data = {5, 4, 3, 2};
-        int el = 5;
-        int result = FindLoop.indexOf(data, el);
-        int expected = 0;
-        Assert.assertEquals(expected, result);
-    }
+        @Test
+        public void whenArrayHas5Then0() {
+            int[] data = {5, 4, 3, 2};
+            int el = 5;
+            int result = FindLoop.indexOf(data, el);
+            int expected = 0;
+            Assert.assertEquals(expected, result);
+        }
 
-    @Test
-    public void whenArrayHas7Then4() {
-        int[] data = {1, 3, 10, 7, 12};
-        int el = 12;
-        int result = FindLoop.indexOf(data, el);
-        int expected = 4;
-        Assert.assertEquals(expected, result);
-    }
+        @Test
+        public void whenArrayHas7Then4() {
+            int[] data = {1, 3, 10, 7, 12};
+            int el = 12;
+            int result = FindLoop.indexOf(data, el);
+            int expected = 4;
+            Assert.assertEquals(expected, result);
+        }
 
-    @Test
-    public void whenArrayHas13Thenminus1() {
-        int[] data = {1, 2, 5, 8};
-        int el = 13;
-        int result = FindLoop.indexOf(data, el);
-        int expected = -1;
-        Assert.assertEquals(expected, result);
-    }
+        @Test
+        public void whenArrayHas13Thenminus1() {
+            int[] data = {1, 2, 5, 8};
+            int el = 13;
+            int result = FindLoop.indexOf(data, el);
+            int expected = -1;
+            Assert.assertEquals(expected, result);
+        }
 
-    @Test
-    public void whenArrayHasLength5Then0() {
-        int[] data = new int[]{5, 10, 3};
-        int el = 5;
-        int result = FindLoop.indexOf(data, el);
-        int expected = 0;
-        Assert.assertEquals(expected, result);
-    }
+        @Test
+        public void whenArrayHasLength5Then0() {
+            int[] data = new int[]{5, 10, 3};
+            int el = 5;
+            int result = FindLoop.indexOf(data, el);
+            int expected = 0;
+            Assert.assertEquals(expected, result);
+        }
 
     @Test
     public void whenFind3() {
