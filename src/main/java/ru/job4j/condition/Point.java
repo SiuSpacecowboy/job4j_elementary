dbc48b14d8428f8184e4876cab8bd6051685a0d2
SiuSpacecowboy <danvitaly14@mail.ru>
2021-12-29 19:31:21 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,13 +1,41 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double d1 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return d1;
+
+    private int x;
+    private int y;
+    private int z;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 
     public static void main(String[] args) {
-       double result =  Point.distance(0, 0, 2, 0);
-       System.out.print("result from the first coordinate to the  second  is " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(0, 2, 3);
+        Point d = new Point(0, 2, 5);
+        double dist = a.distance(b);
+        System.out.println(dist);
+        double dist3d = c.distance3d(d);
+        System.out.println(dist3d);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,46 +8,20 @@ import static org.junit.Assert.*;
 public class PointTest {
 
     @Test
-    public void when00to20then2() {
-        double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void distance() {
+        Point a = new Point(0, 2);
+        Point b = new Point(0, 0);
+        double res = a.distance(b);
+        double exp = 2;
+        Assert.assertEquals(exp, res, 0.001);
     }
 
     @Test
-    public void when11to11then0() {
-        double expected = 0;
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 1;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when23to20then3() {
-        double expected = 3;
-        int x1 = 2;
-        int y1 = 3;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
-
-    @Test
-    public void when1030to22117then211dot40() {
-        double expected = 211.40;
-        int x1 = 10;
-        int y1 = 30;
-        int x2 = 221;
-        int y2 = 17;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+    public void distance3d() {
+        Point a = new Point(0, 2, 0);
+        Point b = new Point(0, 0, 0);
+        double res = a.distance3d(b);
+        double exp = 2;
+        Assert.assertEquals(exp, res, 0.001);
     }
 }
